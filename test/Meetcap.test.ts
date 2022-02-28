8441211aca7d1a9c17ba8bf5396b4efc9a8a1aaf
Flora <anhoa241@gmail.com>
2022-02-28 20:37:57 +0300

    configure sohint & prettier

---
+++ test/Meetcap.test.ts
@@ -0,0 +1,192 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
+import { expect } from 'chai';
+import type { Contract, ContractFactory } from 'ethers';
+import { ethers, deployments } from 'hardhat';
+
+const { BigNumber } = ethers;
+
+describe('CoinBHO', () => {
+  let coinContract: Contract;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+
+  beforeEach(async () => {
+    await deployments.fixture(['coin-bho-v2']);
+    [owner, addr1, addr2] = await ethers.getSigners();
+    [owner, addr1] = await ethers.getSigners();
+    coinContract = await ethers.getContract('CoinBHO');
+    await coinContract.deployed();
+  });
+
+  describe('Deployment', function () {
+    it('Should have 18 decimals', async () => {
+      expect(await coinContract.decimals()).to.equal(18);
+    });
+
+    it('Should have 10B total supply', async () => {
+      expect(await coinContract.totalSupply()).to.equal(
+        BigNumber.from(10_000_000_000).mul(BigNumber.from(10).pow(18))
+      );
+    });
+
+    it('Should set the right owner', async function () {
+      expect(await coinContract.owner()).to.equal(owner.address);
+    });
+
+    it('Should assign the total supply of tokens to the owner', async function () {
+      const ownerBalance = await coinContract.balanceOf(owner.address);
+      expect(await coinContract.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe('Transactions', function () {
+    it('Should transfer tokens between accounts', async function () {
+      // Transfer 50 tokens from owner to addr1
+      const decimals = await coinContract.decimals();
+      const transferAmount = BigNumber.from(10_000_000).mul(
+        BigNumber.from(10).pow(decimals)
+      );
+      await coinContract.transfer(addr1.address, transferAmount);
+      const addr1Balance = await coinContract.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(transferAmount);
+
+      // Transfer 50 tokens from addr1 to addr2
+      // We use .connect(signer) to send a transaction from another account
+      await coinContract.connect(addr1).transfer(addr2.address, transferAmount);
+      const addr2Balance = await coinContract.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(transferAmount);
+    });
+
+    it('Should fail if sender doesn’t have enough tokens', async function () {
+      const initialOwnerBalance = await coinContract.balanceOf(owner.address);
+
+      // Try to send 1 token from addr1 (0 tokens) to owner (1000 tokens).
+      // `require` will evaluate false and revert the transaction.
+      await expect(coinContract.connect(addr1).transfer(owner.address, 1)).to.be
+        .reverted;
+
+      // Owner balance shouldn't have changed.
+      expect(await coinContract.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
+      );
+    });
+
+    it('Should update balances after transfers', async function () {
+      const initialOwnerBalance = await coinContract.balanceOf(owner.address);
+
+      // Transfer 100 tokens from owner to addr1.
+      await coinContract.transfer(addr1.address, 100);
+
+      // Transfer another 50 tokens from owner to addr2.
+      await coinContract.transfer(addr2.address, 50);
+
+      // Check balances.
+      const finalOwnerBalance = await coinContract.balanceOf(owner.address);
+      expect(finalOwnerBalance).to.equal(
+        BigNumber.from(initialOwnerBalance).sub(150)
+      );
+
+      const addr1Balance = await coinContract.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(100);
+
+      const addr2Balance = await coinContract.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+
+    it('should reduce total supply when burn', async function () {
+      const initialOwnerBalance = await coinContract.balanceOf(owner.address);
+      const burnAmount = 1_000_000_000;
+      await coinContract.burn(burnAmount);
+      const totalSupply = await coinContract.totalSupply();
+      expect(totalSupply).to.equal(
+        BigNumber.from(initialOwnerBalance).sub(burnAmount)
+      );
+    });
+
+    it('should revert when burn amount exceeds balance', async function () {
+      const initialOwnerBalance = await coinContract.balanceOf(owner.address);
+      const burnAmount = BigNumber.from(initialOwnerBalance).add(1_000_000);
+      await expect(coinContract.burn(burnAmount)).to.revertedWith(
+        'BEP20: burn amount exceeds balance'
+      );
+    });
+
+    it('should revert when burnFrom amount exceeds allowance', async function () {
+      await coinContract.approve(addr1.address, 100);
+      await expect(coinContract.connect(addr1).burnFrom(owner.address, 200)).to
+        .reverted;
+    });
+
+    it('should burnFrom really burn tokens, reduce allowance, reduce total supply', async function () {
+      const burnAmount = 100;
+      await coinContract.approve(addr1.address, burnAmount);
+      const ownerBalance = await coinContract.balanceOf(owner.address);
+
+      await coinContract.connect(addr1).burnFrom(owner.address, burnAmount);
+      expect(
+        await coinContract.allowance(owner.address, addr1.address)
+      ).to.equal(0);
+      expect(await coinContract.balanceOf(owner.address)).to.equal(
+        BigNumber.from(ownerBalance).sub(burnAmount)
+      );
+      expect(await coinContract.totalSupply()).to.equal(
+        BigNumber.from(ownerBalance).sub(burnAmount)
+      );
+    });
+
+    it('should increase total supply when mint', async function () {
+      const initialOwnerBalance = await coinContract.balanceOf(owner.address);
+      const mintAmount = 1_000_000_000;
+      await coinContract['mint(uint256)'](mintAmount);
+      const totalSupply = await coinContract.totalSupply();
+      expect(totalSupply).to.equal(
+        BigNumber.from(initialOwnerBalance).add(mintAmount)
+      );
+    });
+
+    it('should revert when minting from stranger', async function () {
+      const mintAmount = 1_000_000_000;
+      await expect(
+        coinContract.connect(addr1)['mint(uint256)'](mintAmount)
+      ).to.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('CoinBHOV2 should revert when call initialize_v2 more than once', async function () {
+      await expect(coinContract.initialize_v2()).to.revertedWith(
+        'CoinBHOV2: already initialized'
+      );
+    });
+
+    it('CoinBHOV2: only minter can mint', async function () {
+      const mintAmount = 1_000_000_000;
+      await coinContract.grantRole(coinContract.MINTER_ROLE(), owner.address);
+      await expect(
+        coinContract
+          .connect(owner)
+          ['mint(address,uint256)'](owner.address, mintAmount)
+      ).to.not.reverted;
+
+      await expect(
+        coinContract
+          .connect(addr1)
+          ['mint(address,uint256)'](addr1.address, mintAmount)
+      ).to.reverted;
+    });
+
+    it('CoinBHOV2: should increase total supply when mint', async function () {
+      const mintAmount = 1_000_000_000;
+      const initialOwnerBalance = await coinContract.balanceOf(owner.address);
+      await coinContract.grantRole(coinContract.MINTER_ROLE(), owner.address);
+      await expect(
+        coinContract
+          .connect(owner)
+          ['mint(address,uint256)'](owner.address, mintAmount)
+      ).to.not.reverted;
+      const totalSupply = await coinContract.totalSupply();
+      expect(totalSupply).to.equal(
+        BigNumber.from(initialOwnerBalance).add(mintAmount)
+      );
+    });
+  });
+});
---
+++ test/MeetcapTimeLock.test.ts
@@ -0,0 +1,421 @@
+import { expect } from 'chai';
+import { ethers, waffle, deployments } from 'hardhat';
+import { Contract, BigNumber } from 'ethers';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
+import { EthUtils, daysToSeconds } from '../../utils';
+
+describe('TokenTimeLock', function () {
+  let coinContract: Contract;
+  let lockContract: Contract;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let factoryAddr: SignerWithAddress;
+
+  beforeEach(async function () {
+    await deployments.fixture(['token-time-lock', 'coin-bho']);
+    [owner, addr1, factoryAddr] = await ethers.getSigners();
+    coinContract = await ethers.getContract('CoinBHO');
+    lockContract = await ethers.getContract('TokenTimeLock');
+    await coinContract.deployed();
+    await lockContract.deployed();
+  });
+
+  describe('register new lock', function () {
+    it('new lock should be stored successfully', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        10,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 20, 20, 20],
+        await EthUtils.latestBlockTimestamp()
+      );
+      const [
+        user,
+        token,
+        amount,
+        releasedAmount,
+        startDate,
+        lockDurations,
+        releasePercents,
+        releaseDates,
+        nextReleaseIdx,
+        factory,
+      ] = await lockContract.lockData();
+
+      expect(amount).to.equal(10);
+      expect(lockDurations).to.deep.equal([
+        daysToSeconds(1),
+        daysToSeconds(2),
+        daysToSeconds(3),
+        daysToSeconds(4),
+        daysToSeconds(5),
+      ]);
+      expect(releasePercents).to.deep.equal([20, 20, 20, 20, 20]);
+      expect(nextReleaseIdx).to.equal(0);
+      expect(releasedAmount).to.equal(0);
+      expect(factory).to.equal(factoryAddr.address);
+    });
+
+    it('should revert when unlock percents and unlock dates length not match', async function () {
+      await expect(
+        lockContract.initialize(
+          factoryAddr.address,
+          addr1.address,
+          coinContract.address,
+          10,
+          [
+            daysToSeconds(1),
+            daysToSeconds(2),
+            daysToSeconds(3),
+            daysToSeconds(4),
+            daysToSeconds(5),
+          ],
+          [20, 20, 20, 20],
+          await EthUtils.latestBlockTimestamp()
+        )
+      ).to.revertedWith('TokenTimeLock: unlock length not match');
+    });
+
+    it('should revert when total unlock percents not 100', async function () {
+      await expect(
+        lockContract.initialize(
+          factoryAddr.address,
+          addr1.address,
+          coinContract.address,
+          10,
+          [
+            daysToSeconds(1),
+            daysToSeconds(2),
+            daysToSeconds(3),
+            daysToSeconds(4),
+            daysToSeconds(5),
+          ],
+          [20, 20, 20, 20, 10],
+          await EthUtils.latestBlockTimestamp()
+        )
+      ).to.revertedWith('TokenTimeLock: unlock percent not match 100');
+    });
+
+    it('should revert when call initialize() more than once', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        10,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 20, 20, 20],
+        await EthUtils.latestBlockTimestamp()
+      );
+
+      await expect(
+        lockContract.initialize(
+          factoryAddr.address,
+          addr1.address,
+          coinContract.address,
+          10,
+          [
+            daysToSeconds(1),
+            daysToSeconds(2),
+            daysToSeconds(3),
+            daysToSeconds(4),
+            daysToSeconds(5),
+          ],
+          [20, 20, 20, 20, 20],
+          await EthUtils.latestBlockTimestamp()
+        )
+      ).to.reverted;
+    });
+
+    it('should set factory correctly when initialize()', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        10,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 20, 20, 20],
+        await EthUtils.latestBlockTimestamp()
+      );
+
+      expect(await lockContract.factory()).to.equal(factoryAddr.address);
+    });
+  });
+
+  describe('release locked tokens', function () {
+    it('should release locked tokens to correct user when unlock conditions are met', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        BigNumber.from(100_000_000).mul(
+          BigNumber.from(10).pow(BigNumber.from(await coinContract.decimals()))
+        ),
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 10, 25, 25],
+        await EthUtils.latestBlockTimestamp()
+      );
+      await coinContract.transfer(
+        lockContract.address,
+        BigNumber.from(100_000_000).mul(
+          BigNumber.from(10).pow(BigNumber.from(await coinContract.decimals()))
+        )
+      );
+      const decimals = await coinContract.decimals();
+
+      // Release 1st phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24]);
+      await lockContract.release();
+      expect(await coinContract.balanceOf(addr1.address)).to.equal(
+        BigNumber.from(20_000_000).mul(
+          BigNumber.from(10).pow(BigNumber.from(decimals))
+        )
+      );
+
+      // Release 2nd 3rd phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24 * 2]);
+      await lockContract.release();
+      expect(await coinContract.balanceOf(addr1.address)).to.equal(
+        BigNumber.from(50_000_000).mul(
+          BigNumber.from(10).pow(BigNumber.from(decimals))
+        )
+      );
+
+      // Release remaining phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24 * 2]);
+      await lockContract.release();
+      expect(await coinContract.balanceOf(addr1.address)).to.equal(
+        BigNumber.from(100_000_000).mul(
+          BigNumber.from(10).pow(BigNumber.from(decimals))
+        )
+      );
+    });
+
+    it('should emit Release event with correct data', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        100,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 10, 25, 25],
+        await EthUtils.latestBlockTimestamp()
+      );
+
+      await coinContract.transfer(lockContract.address, 100);
+
+      const releaseDates = [
+        (await EthUtils.latestBlockTimestamp()) + daysToSeconds(1),
+        (await EthUtils.latestBlockTimestamp()) + daysToSeconds(5),
+      ];
+
+      // Move to 1st phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24]);
+      await expect(lockContract.connect(addr1).release())
+        .to.emit(lockContract, 'Released')
+        .withArgs(20, 20, 0, 0, releaseDates[0]);
+
+      // Move to last phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24 * 4]);
+      await expect(lockContract.connect(addr1).release())
+        .to.emit(lockContract, 'Released')
+        .withArgs(80, 100, 1, 4, releaseDates[1]);
+    });
+
+    it('just to check what maximum gas that release all phases at a time', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        100,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 10, 25, 25],
+        await EthUtils.latestBlockTimestamp()
+      );
+
+      await coinContract.transfer(lockContract.address, 100);
+
+      ethers.provider.send('evm_increaseTime', [3600 * 24 * 10]);
+      await lockContract.release();
+
+      expect(await coinContract.balanceOf(addr1.address)).to.equal(100);
+    });
+
+    it('should set release dates correct', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        100,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 10, 25, 25],
+        await EthUtils.latestBlockTimestamp()
+      );
+
+      await coinContract.transfer(lockContract.address, 100);
+
+      const originDate = await EthUtils.latestBlockTimestamp();
+      const expectedReleaseDates = [
+        originDate + daysToSeconds(1),
+        originDate + daysToSeconds(5),
+        originDate + daysToSeconds(5),
+        originDate + daysToSeconds(5),
+        originDate + daysToSeconds(5),
+      ];
+
+      // Move to 1st phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24]);
+      await lockContract.connect(addr1).release();
+      const [_1, _2, _3, _4, _5, _6, _7, releaseDates] =
+        await lockContract.lockData();
+      expect(releaseDates[0]).to.equal(expectedReleaseDates[0]);
+
+      // Move to last phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24 * 4]);
+      await lockContract.connect(addr1).release();
+      const [__1, __2, __3, __4, __5, __6, __7, releaseDates_2] =
+        await lockContract.lockData();
+      expect(releaseDates_2[0]).to.equal(expectedReleaseDates[0]);
+      expect(releaseDates_2[1]).to.equal(expectedReleaseDates[1]);
+      expect(releaseDates_2[2]).to.equal(expectedReleaseDates[2]);
+      expect(releaseDates_2[3]).to.equal(expectedReleaseDates[3]);
+      expect(releaseDates_2[4]).to.equal(expectedReleaseDates[4]);
+    });
+
+    it(`should revert when does not meet next unlock phase requirements`, async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        10,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 10, 25, 25],
+        await EthUtils.latestBlockTimestamp()
+      );
+
+      await coinContract.transfer(lockContract.address, 100);
+
+      await expect(lockContract.connect(addr1).release()).to.be.revertedWith(
+        'TokenTimeLock: next phase unavailable'
+      );
+
+      // Release 1st phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24]);
+      await lockContract.connect(addr1).release();
+
+      // Should revert if users try to release 2nd phase
+      await expect(lockContract.connect(addr1).release()).to.be.revertedWith(
+        'TokenTimeLock: next phase unavailable'
+      );
+
+      // Release 2nd, 3rd, 4th phase
+      ethers.provider.send('evm_increaseTime', [3600 * 24 * 3]);
+      await lockContract.connect(addr1).release();
+
+      // Should revert if users try to release 5th phase
+      await expect(lockContract.connect(addr1).release()).to.be.revertedWith(
+        'TokenTimeLock: next phase unavailable'
+      );
+    });
+
+    it('should revert when all lock phases are released', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        10,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 10, 25, 25],
+        await EthUtils.latestBlockTimestamp()
+      );
+      await coinContract.transfer(lockContract.address, 100);
+
+      // Release all phases
+      ethers.provider.send('evm_increaseTime', [3600 * 24 * 5]);
+      await lockContract.connect(addr1).release();
+
+      await expect(lockContract.connect(addr1).release()).to.be.revertedWith(
+        'TokenTimeLock: all phases are released'
+      );
+    });
+
+    it('should revert when contract has insufficient balance to withdraw', async function () {
+      await lockContract.initialize(
+        factoryAddr.address,
+        addr1.address,
+        coinContract.address,
+        100,
+        [
+          daysToSeconds(1),
+          daysToSeconds(2),
+          daysToSeconds(3),
+          daysToSeconds(4),
+          daysToSeconds(5),
+        ],
+        [20, 20, 10, 25, 25],
+        await EthUtils.latestBlockTimestamp()
+      );
+      await coinContract.transfer(lockContract.address, 50);
+      // Release all phases
+      ethers.provider.send('evm_increaseTime', [3600 * 24 * 5]);
+      await expect(lockContract.connect(addr1).release()).to.be.revertedWith(
+        'TokenTimeLock: insufficient balance'
+      );
+    });
+  });
+});
